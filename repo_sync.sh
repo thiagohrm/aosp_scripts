0ef318dc85e6355149d45a24ad256d8de0b1a8e8
Thiago Mattos <thiagohrm@gmail.com>
2022-12-18 20:53:25 -0300

    Create repo_sync.sh

---
+++ repo_sync.sh
@@ -0,0 +1,7 @@
+#!/bin/bash +x
+aosp_path="${WORKSPACE}/aosp"
+cd ${aosp_path}
+git config --global user.name Your Name
+git config --global user.email you@example.com
+repo init -u https://android.googlesource.com/platform/manifest -b android-9.0.0_r1
+repo sync -cd -j4
