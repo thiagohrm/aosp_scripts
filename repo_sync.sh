2afaeae3f62990e1d30225e72838d3e56f5c4283
Thiago Mattos <thiagohrm@gmail.com>
2022-12-18 21:09:55 -0300

    Update repo_sync.sh

--- repo_sync.sh
+++ repo_sync.sh
@@ -1,8 +1,7 @@
 #!/bin/bash +x
-aosp_path="${WORKSPACE}"
-cd ${aosp_path}
+cd ${WORKSPACE}
 mkdir aosp
-cd /aosp
+cd aosp
 git config --global user.name Your Name
 git config --global user.email you@example.com
 repo init -u https://android.googlesource.com/platform/manifest -b android-9.0.0_r1
