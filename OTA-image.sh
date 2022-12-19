186599fb471c0f4df1063b5e41777f2b62dada1f
Thiago Mattos <thiagohrm@gmail.com>
2022-12-18 21:18:31 -0300

    Update OTA-image.sh

--- OTA-image.sh
+++ OTA-image.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -el
 productName=$1
 flavour=$2
-aosp_path="${WORKSPACE}/aosp"
 
 OTA_build() { # Needs previous system image to be present.
     echo "**********************Initiating OTA Build**********************"
     ./build/tools/releasetools/ota_from_target_files dist_output/${productName}-target_files-${flavour}.zip dist_output/full_ota_${flavour}.zip
 }
 
-cd ${aosp_path}
+cd ${WORKSPACE}/aosp
 OTA_Build
