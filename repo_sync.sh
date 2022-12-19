7121a7a2c40cea66079ffdc3df03b19c87b87916
Thiago Mattos <thiagohrm@gmail.com>
2022-12-19 16:51:10 -0300

    Update repo_sync.sh

--- repo_sync.sh
+++ repo_sync.sh
@@ -5,4 +5,4 @@ cd aosp
 git config --global user.name Your Name
 git config --global user.email you@example.com
 repo init -u https://android.googlesource.com/platform/manifest -b android-9.0.0_r1
-repo sync -cd -j4
+repo sync -j8
